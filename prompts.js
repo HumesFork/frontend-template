ada8b65679b7007b7f66a5841edbdcf954057265
js design patterns <31274733+HumesFork@users.noreply.github.com>
2022-05-06 17:34:27 +0800

    Add files via upload

---
+++ prompts.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    name: "elementUI",
+    type: "confirm",
+    message: `element-ui`,
+    default: false,
+  },
+  {
+    type: "input", // 类型为 输入项
+    name: "sidebar_localhost",
+    message: "请输入侧边栏本地跳转IP",
+    default: "http://10.1.52.7",
+  },
+];
